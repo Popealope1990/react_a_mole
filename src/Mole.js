ffa70146ec5345143a2d0eaba7fec762bda309ba
Benjamin Pope <buddypope1234@gmail.com>
2022-12-03 06:41:11 -0600

    initial commit as well as mole and mole container are built

---
+++ src/Mole.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Mole() {
+    return(
+        <h2>I am a Mole</h2>
+    )
+}
+
+export default Mole
---
+++ src/MoleContainer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Mole from './Mole'
+
+function MoleContainer() {
+    return(
+        <div>
+            <h2>This is a Mole container</h2>
+            <Mole/>
+        </div>
+    )
+}
+
+export default MoleContainer
